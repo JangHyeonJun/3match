fbbf8819ae297154e184d88c6a1dc5c841315775
JangHyeonjun <totok682@naver.com>
2019-12-04 18:30:34 +0900

    automatically generate tiles without repeating tiles

--- Match-3-Game-Starter/Match 3 Game Starter/Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
+++ Match-3-Game-Starter/Match 3 Game Starter/Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -46,11 +46,25 @@ public class BoardManager : MonoBehaviour {
 
         float startX = transform.position.x;
 		float startY = transform.position.y;
+        Sprite[] previousLeft = new Sprite[ySize];
+        Sprite previousBelow = null;
 
-		for (int x = 0; x < xSize; x++) {
+        for (int x = 0; x < xSize; x++) {
 			for (int y = 0; y < ySize; y++) {
 				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
 				tiles[x, y] = newTile;
+                newTile.transform.parent = transform;
+
+                List<Sprite> possibleCharacters = new List<Sprite>();
+                possibleCharacters.AddRange(characters);
+                possibleCharacters.Remove(previousLeft[y]);
+                possibleCharacters.Remove(previousBelow);
+
+                Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                previousLeft[y] = newSprite;
+                previousBelow = newSprite;
 			}
         }
     }
