7bbf9f8af93141a4078ff73a301c32132ad48477
JangHyeonjun <totok682@naver.com>
2019-12-04 18:45:19 +0900

    swap tile condition check (adjacent tiles, selecting, deselecting, matching) clear tiles

--- Match-3-Game-Starter/Match 3 Game Starter/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
+++ Match-3-Game-Starter/Match 3 Game Starter/Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -33,6 +33,8 @@ public class Tile : MonoBehaviour {
 
 	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
 
+    private bool matchFound = false;
+
 	void Awake() {
 		render = GetComponent<SpriteRenderer>();
     }
@@ -49,5 +51,99 @@ public class Tile : MonoBehaviour {
 		render.color = Color.white;
 		previousSelected = null;
 	}
+
+    private void OnMouseDown()
+    {
+        if (render.sprite == null || BoardManager.instance.IsShifting)
+            return;
+
+        if (isSelected)
+            Deselect();
+        else
+        {
+            if (previousSelected == null)
+                Select();
+            else
+            {
+                if(GetAllAdjacentTiles().Contains(previousSelected.gameObject))
+                {
+                    SwapSprite(previousSelected.render);
+                    previousSelected.ClearAllMatches();
+                    previousSelected.Deselect();
+                    ClearAllMatches();
+                }
+                else
+                {
+                    previousSelected.GetComponent<Tile>().Deselect();
+                    Select();
+                }
+            }
+        }
+    }
+
+    public void SwapSprite(SpriteRenderer render2)
+    {
+        if (render.sprite == render2.sprite)
+            return;
+
+        Sprite tempSprite = render2.sprite;
+        render2.sprite = render.sprite;
+        render.sprite = tempSprite;
+        SFXManager.instance.PlaySFX(Clip.Swap);
+    }
+
+    private GameObject GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        if (hit.collider != null)
+            return hit.collider.gameObject;
+        return null;
+    }
+
+    private List<GameObject> GetAllAdjacentTiles()
+    {
+        List<GameObject> adjacentTiles = new List<GameObject>();
+        for (int i = 0; i < adjacentDirections.Length; i++)
+            adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+        return adjacentTiles;
+    }
+
+    private List<GameObject> FindMatch(Vector2 castDir) {
+        List<GameObject> matchingTiles = new List<GameObject>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        while (hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == render.sprite){
+            matchingTiles.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, castDir);
+        }
+        return matchingTiles;
+    }
+
+    private void ClearMatch(Vector2[] paths)
+    {
+        List<GameObject> matchingTiles = new List<GameObject>();
+        for (int i = 0; i < paths.Length; i++)
+            matchingTiles.AddRange(FindMatch(paths[i]));
+        if(matchingTiles.Count >= 2)
+        {
+            for (int i = 0; i < matchingTiles.Count; i++)
+                matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
+            matchFound = true;
+        }
+    }
+
+    public void ClearAllMatches()
+    {
+        if (render.sprite == null)
+            return;
+
+        ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+        ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+        if (matchFound)
+        {
+            render.sprite = null;
+            matchFound = false;
+            SFXManager.instance.PlaySFX(Clip.Clear);
+        }
+    }
 
 }
